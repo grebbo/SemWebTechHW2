4b0ad4996dd01419ef95dc9e9f137cdf3f2ca067
LAPTOP-GCOIJDKD\Eghome <enrife93@gmail.com>
2017-02-04 15:10:25 +0100

    Prima bozza (per l'ignore non so cosa mettere sinceramente)

---
+++ src/Homework2/Pizza2Person.java
@@ -0,0 +1,11 @@
+package Homework2;
+
+public class Pizza2Person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//relazione di corrispondenza tra le persone (scorso homework) e pizze (definite prima)
+	}
+
+}
---
+++ src/Homework2/PizzaSearch.java
@@ -0,0 +1,18 @@
+package Homework2;
+
+public class PizzaSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//registro i dati
+		
+		//faccio la query su dbpedia e un altro dataset
+		
+		//mostro il resultset come json o xml/rdf o html
+	}
+
+	private class Model {
+		//model per i dati delle pizze
+	}
+}
