4edb362815d01017a64b99d577917569945b00c8
LAPTOP-GCOIJDKD\Eghome <enrife93@gmail.com>
2017-02-08 12:32:33 +0100

    Query ano fatta

--- src/Tutorials/ExampleDBPedia2.java
+++ src/Tutorials/ExampleDBPedia2.java
@@ -32,7 +32,7 @@ public class ExampleDBPedia2 {
 	        String queryString = 
 	            "SELECT * WHERE { " +
 	            "    SERVICE <http://dbpedia-live.openlinksw.com/sparql?timeout=2000> { " +
-	            "        SELECT DISTINCT ?company where {?company a <http://dbpedia.org/ontology/Company>} LIMIT 20" +
+	            "        SELECT DISTINCT ?name where {?name a <http://dbpedia.org/ontology/Name> . FILTER regex(?name, \"ano\", \"i\")} LIMIT 20" +
 	            "    }" +
 	            "}" ;
 	        Query query = QueryFactory.create(queryString) ;
