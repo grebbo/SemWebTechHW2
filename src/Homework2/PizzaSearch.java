179d3f854b0c3e2f417456df107823ebf6059f5e
LAPTOP-GCOIJDKD\Eghome <enrife93@gmail.com>
2017-02-10 15:51:25 +0100

    ghk

--- src/Homework2/PizzaSearch.java
+++ src/Homework2/PizzaSearch.java
@@ -1,11 +1,35 @@
 package Homework2;
 
+import java.util.ArrayList;
+
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.vocabulary.OWL;
+import org.apache.jena.vocabulary.RDFS;
+
 public class PizzaSearch {
+	
+	static public final String NL = System.getProperty("line.separator");
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//registro i dati
+		String prolog = "PREFIX owl: <" + OWL.getURI() + ">";
+		String prolog2 = "PREFIX rdfs: <" + RDFS.getURI() + ">";
+		
+		OntModel myOntologyModel = ModelFactory.createOntologyModel();
+		myOntologyModel.read("pizza.owl");
+		
+		//System.out.println(myOntologyModel.toString());
+		//sembra funzionare a giudicare dal print
+		
+		ArrayList<String> namedPizzas = new ArrayList<String>();
+		
+		String queryString = prolog + NL + prolog2 + NL
+				+ " SELECT ?name "
+				+ "	WHERE "
+				+ " {	 [] a owl:Class:NamedPizza "
+				+ " } ";
 		
 		//faccio la query su dbpedia e un altro dataset
 		
