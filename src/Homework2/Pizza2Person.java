8252fc9933de6656430d2d249c8455e4f3039643
LAPTOP-GCOIJDKD\Eghome <enrife93@gmail.com>
2017-02-15 16:20:22 +0100

    Ghe semu quasi

--- src/Homework2/Pizza2Person.java
+++ src/Homework2/Pizza2Person.java
@@ -1,11 +1,117 @@
 package Homework2;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSetFactory;
+import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.query.ResultSetRewindable;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.vocabulary.OWL;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
+
 public class Pizza2Person {
 
-	public static void main(String[] args) {
+	static public final String NL = System.getProperty("line.separator");
+
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		//relazione di corrispondenza tra le persone (scorso homework) e pizze (definite prima)
-	}
+		// relazione di corrispondenza tra le persone (scorso homework) e pizze
+		// (definite prima)
+		String prologOwl = "PREFIX owl: <" + OWL.getURI() + ">";
+		String prologRdfs = "PREFIX rdfs: <" + RDFS.getURI() + ">";
+		String prologRdf = "PREFIX rdf: <" + RDF.getURI() + ">";
+		String prologDbo = "PREFIX dbo: <http://dbpedia.org/ontology/ingredient>";
+		String pizzaOntology = "PREFIX pizzaOnt: <http://www.co-ode.org/ontologies/pizza/pizza.owl#>";
 
+		/*
+		 * Model myOntologyModel = ModelFactory.createOntologyModel();
+		 * myOntologyModel.read("pizza.owl");
+		 * System.out.println(myOntologyModel.toString());
+		 */
+
+		String queryStr = "";
+		QueryExecution qexec = null;
+		Query query = null;
+		ResultSetRewindable rs = null;
+
+		ArrayList<String> namedPizzas = new ArrayList<String>();
+
+		try {
+			queryStr = prologRdfs + NL + pizzaOntology + NL + " SELECT * " + " WHERE { "
+					+ "		?pizza <http://www.w3.org/2000/01/rdf-schema#subClassOf> pizzaOnt:NamedPizza " + " }";
+
+			query = QueryFactory.create(queryStr);
+			Model model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_RULE_INF);
+			model.read("pizza.owl");
+
+			qexec = QueryExecutionFactory.create(query, model);
+
+			rs = ResultSetFactory.makeRewindable(qexec.execSelect());
+
+			rs.next();
+			while (rs.hasNext()) {
+				String t = rs.next().toString();
+				namedPizzas.add(t.substring(t.indexOf("<") + 1, t.indexOf(">")));
+
+			}
+			System.out.println(namedPizzas);
+			qexec.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		InputStream in = new FileInputStream(new File("mycontacts.rdf"));
+
+		// Create an empty in-memory model and populate it from the graph
+		Model model = ModelFactory.createMemModelMaker().createDefaultModel();
+		model.read(in, null); // null base URI, since model URIs are absolute
+		in.close();
+		
+		ArrayList<String> personIDs = new ArrayList<String>();
+		
+		try {
+			queryStr = prologRdf + NL + "PREFIX foaf: <http://xmlns.com/foaf/0.1/> " + NL
+					+ " SELECT ?id "
+					+ " WHERE { "
+					+ " 	?id a foaf:Agent " 
+					+ " }";
+
+			query = QueryFactory.create(queryStr);
+			qexec = QueryExecutionFactory.create(query, model);
+
+			rs = ResultSetFactory.makeRewindable(qexec.execSelect());
+
+			rs.next();
+			while (rs.hasNext()) {
+				String t = rs.next().toString();
+				personIDs.add(t.substring(t.indexOf("=")+1, t.indexOf(")")).trim());
+			}
+			System.out.println(personIDs);
+			rs.reset();
+			//ResultSetFormatter.outputAsCSV(rs);
+			qexec.close();
+
+			System.out.println(model.createList(model.listResourcesWithProperty(model.getProperty("foaf:name"))));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//model.add(giuseppe, likes, sloppy)
+
+	}
 }
--- src/Homework2/PizzaSearch.java
+++ src/Homework2/PizzaSearch.java
@@ -9,7 +9,9 @@ import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFactory;
 import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.query.ResultSetRewindable;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
@@ -19,139 +21,153 @@ import org.apache.jena.vocabulary.OWL;
 import org.apache.jena.vocabulary.RDFS;
 import org.apache.jena.vocabulary.RDF;
 
-
 public class PizzaSearch {
 
-  static public final String NL = System.getProperty("line.separator");
+	static public final String NL = System.getProperty("line.separator");
 
-  public static void main(String[] args) throws Exception {
-    // TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
 
-    String prologOwl = "PREFIX owl: <" + OWL.getURI() + ">";
-    String prologRdfs = "PREFIX rdfs: <" + RDFS.getURI() + ">";
-    String prologRdf = "PREFIX rdf: <" + RDF.getURI() + ">";
-    String pizzaOntology = "PREFIX pizzaOnt: <http://www.co-ode.org/ontologies/pizza/pizza.owl#>";
-    
+		String prologOwl = "PREFIX owl: <" + OWL.getURI() + ">";
+		String prologRdfs = "PREFIX rdfs: <" + RDFS.getURI() + ">";
+		String prologRdf = "PREFIX rdf: <" + RDF.getURI() + ">";
+		String prologDbo = "PREFIX dbo: <http://dbpedia.org/ontology/ingredient>";
+		String pizzaOntology = "PREFIX pizzaOnt: <http://www.co-ode.org/ontologies/pizza/pizza.owl#>";
 
-    /*Model myOntologyModel = ModelFactory.createOntologyModel();
-    myOntologyModel.read("pizza.owl");
-    System.out.println(myOntologyModel.toString());*/
+		/*
+		 * Model myOntologyModel = ModelFactory.createOntologyModel();
+		 * myOntologyModel.read("pizza.owl");
+		 * System.out.println(myOntologyModel.toString());
+		 */
 
-    String queryStr = "";
-    QueryExecution qexec = null;
-    Query query = null;
-    ResultSet rs = null;
+		String queryStr = "";
+		QueryExecution qexec = null;
+		Query query = null;
+		ResultSetRewindable rs = null;
 
-    ArrayList<String> namedPizzas = new ArrayList<String>();
-    
-    
-    try {
-        queryStr = prologRdfs + NL + pizzaOntology + NL +
-        		 " SELECT * "
-        		+ " WHERE { "
-           		+ "		?pizza <http://www.w3.org/2000/01/rdf-schema#subClassOf> pizzaOnt:NamedPizza "
-        		+ " }";
-        
-        query = QueryFactory.create(queryStr);
-        Model model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_RULE_INF);
-        model.read("pizza.owl");
-        
-       	qexec = QueryExecutionFactory.create(query, model); 
-        
-        rs = qexec.execSelect();
-        //ResultSetFormatter.out(System.out, rs, query);
-        rs.next();
-        while (rs.hasNext()) {
-        	String t = rs.next().toString();
-        namedPizzas.add(t.substring(t.indexOf("#")+1, t.indexOf(">")));
-        }
-        
-        
-        qexec.close();
-        System.out.println(namedPizzas);
-    
- 
-        ArrayList<Pizza> pizzas = new ArrayList<Pizza>();
-        String str = "{?pizza foaf:name\"zzz\"}";
-      for(String pizza:namedPizzas)
-      {
-    	  pizza = stripCapital(pizza);
-    	  str += "UNION" + "{?pizza foaf:name \""+ pizza + "\"@en }";
-      }
-      
-      str += ".";
-      
-     
-      queryStr = prologRdfs + NL + prologRdf + NL + "PREFIX foaf: <http://xmlns.com/foaf/0.1/>" + NL 
-          + " SELECT ?label "
-          + " WHERE { "
-          + "   ?pizza rdfs:label ?label {"
-          + "   SELECT ?pizza " 
-          + "   WHERE {" 
-          + "     ?pizza rdf:type <http://dbpedia.org/ontology/Food> ."
-          + str
-          + "   }}"
-          + " }"  ;
-      
-      query = QueryFactory.create(queryStr);
+		ArrayList<String> namedPizzas = new ArrayList<String>();
 
-      // Remote execution.
-      qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
-      // Set the DBpedia specific timeout.
-      ((QueryEngineHTTP) qexec).addParam("timeout", "100000");
- // Execute.
-      
-      rs = qexec.execSelect();
-      ResultSetFormatter.out(System.out, rs, query);
-      qexec.close();
-      
+		try {
+			queryStr = prologRdfs + NL + pizzaOntology + NL + " SELECT * " + " WHERE { "
+					+ "		?pizza <http://www.w3.org/2000/01/rdf-schema#subClassOf> pizzaOnt:NamedPizza " + " }";
 
-      
-      
-    }
-    catch (Exception e) {
-        e.printStackTrace();
-      }
+			query = QueryFactory.create(queryStr);
+			Model model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_RULE_INF);
+			model.read("pizza.owl");
 
-    // mostro il resultset come json o xml/rdf o html
-  }
+			qexec = QueryExecutionFactory.create(query, model);
 
+			rs =  ResultSetFactory.makeRewindable(qexec.execSelect());
+			// ResultSetFormatter.out(System.out, rs, query);
+			rs.next();
+			while (rs.hasNext()) {
+				String t = rs.next().toString();
+				namedPizzas.add(t.substring(t.indexOf("#") + 1, t.indexOf(">")));
+			}
 
-  public static Model createModel(int resNumber, ArrayList<Pizza> pizzas) {
-    Model m = ModelFactory.createDefaultModel();
+			qexec.close();
+			//System.out.println(namedPizzas);
 
-    String temp = "http://example.org/pizza#";
-    
-    for (int i = 0 ; i < resNumber ; i++) {
-      Resource rTemp = m.createResource(temp+i);
-      addProperties(rTemp, pizzas.get(i));
-    }
-    return m;
-  }
-  
-  public static void addProperties(Resource res, Pizza pizza){
-    //fillare la resource con i campi di pizza ricavati dalla query
-  }
-  
-  public static String stripCapital(String str){
-	    for (int i = 1; i < str.length(); i++)
-	    {
-	    	if (!Character.isAlphabetic(str.charAt(i)))
-	    	{
-	    		i++;
-	    		continue;
-	    	}
-	    	if (Character.isUpperCase(str.charAt(i)))
-	    	{
-	    		str = str.substring(0, i) + " " + str.substring(i);
-	    		i++;
-	    	}
-	    }
-	    return str;
-	  }
-	  
-  
-  private class Pizza {
-        
-  }
+			String str = "{?pizza foaf:name\"zzz\"}";
+			for (String pizza : namedPizzas) {
+				pizza = stripCapital(pizza);
+				str += "UNION" + "{?pizza foaf:name \"" + pizza + "\"@en }";
+			}
+			for (String pizza : namedPizzas) {
+				pizza = stripCapital(pizza).toLowerCase();
+				str += "UNION" + "{?pizza rdfs:label \"Pizza " + pizza + "\"@it }";
+			}
+
+			str += ".";
+			//System.out.println(str);
+			queryStr = prologRdfs + NL + prologRdf + NL + prologDbo + NL + "PREFIX foaf: <http://xmlns.com/foaf/0.1/>"
+					+ NL + " SELECT ?label ?ingredient" + " WHERE {"
+					+ "	?pizza <http://dbpedia.org/property/mainIngredient> ?ingredient . "
+					+ "   ?pizza rdfs:label ?label {" + "   SELECT ?pizza " + "   WHERE {"
+					+ "     ?pizza rdf:type <http://dbpedia.org/ontology/Food> ." + str + "   }}" + " }";
+
+			query = QueryFactory.create(queryStr);
+
+			// Remote execution.
+			qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
+			// Set the DBpedia specific timeout.
+			((QueryEngineHTTP) qexec).addParam("timeout", "100000");
+			// Execute.
+
+			rs =  ResultSetFactory.makeRewindable(qexec.execSelect());
+			System.out.println("Result as CSV: ");
+			ResultSetFormatter.outputAsCSV(rs);
+			rs.reset();
+			System.out.println("Result as JSON: ");
+			ResultSetFormatter.outputAsJSON(rs);
+			rs.reset();
+			System.out.println("Result standard: ");
+			ResultSetFormatter.out(System.out, rs, query);
+			qexec.close();
+			
+			/*
+			str = "{?pizza foaf:name\"zzz\"}";
+			for (String pizza : namedPizzas) {
+				pizza = stripCapital(pizza);
+				str += "UNION" + "{?pizza foaf:name \"" + pizza + "\"@en }";
+			}
+			
+
+			str += ".";
+			*/
+			System.out.println("QUERY SU DBPEDIA(IT)");
+			
+			queryStr = prologRdfs + NL + prologRdf + NL + prologDbo + NL + "PREFIX foaf: <http://xmlns.com/foaf/0.1/>"
+					+ NL 
+					+" SELECT ?label ?region" + " WHERE {"
+					//+ "	OPTIONAL (?pizza <http://dbpedia.org/property/mainIngredient> ?ingredient) . "
+					+ "   ?pizza rdfs:label ?label ."
+					+ "   ?pizza <http://it.dbpedia.org/property/regione> ?region {" 
+					+ "   SELECT ?pizza " + "   WHERE {"
+					+ "     ?pizza rdf:type <http://dbpedia.org/ontology/Food> "
+					+ "." + str 
+					+ "   }"
+					+ "}"
+					+ " }";
+
+			query = QueryFactory.create(queryStr);
+
+			// Remote execution.
+			qexec = QueryExecutionFactory.sparqlService("http://it.dbpedia.org/sparql", query);
+			// Set the DBpedia specific timeout.
+			((QueryEngineHTTP) qexec).addParam("timeout", "100000");
+			// Execute.
+
+			rs =  ResultSetFactory.makeRewindable(qexec.execSelect());
+			System.out.println("Result as CSV: ");
+			ResultSetFormatter.outputAsCSV(rs);
+			rs.reset();
+			System.out.println("Result as JSON: ");
+			ResultSetFormatter.outputAsJSON(rs);
+			rs.reset();
+			System.out.println("Result standard: ");
+			ResultSetFormatter.out(System.out, rs, query);
+			qexec.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// mostro il resultset come json o xml/rdf o html
+	}
+
+	public static String stripCapital(String str) {
+		for (int i = 1; i < str.length(); i++) {
+			if (!Character.isAlphabetic(str.charAt(i))) {
+				i++;
+				continue;
+			}
+			if (Character.isUpperCase(str.charAt(i))) {
+				str = str.substring(0, i) + " " + str.substring(i);
+				i++;
+			}
+		}
+		return str;
+	}
+
 }
